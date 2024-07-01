c9db34f325133a6ba75c7c4150ae6553ff05533f
gesen2egee <79357052+gesen2egee@users.noreply.github.com>
2024-07-01 22:56:17 +0800

    Update caption.py

--- caption.py
+++ caption.py
@@ -19,17 +19,19 @@ def download_file(url, filename):
 def run_setup_script():
     setup_url = "https://raw.githubusercontent.com/gesen2egee/dataset_tools/main/setup.py"
     setup_filename = "setup.py"
+    main_script_filename = "main_script.py"
     
-    if not os.path.exists(setup_filename):
+    if not os.path.exists(setup_filename) or args.upgrade:
         download_file(setup_url, setup_filename)
-    
-    subprocess.run([sys.executable, setup_filename], check=True)
+    if not os.path.exists(main_script_filename) or args.upgrade:
+        subprocess.run([sys.executable, setup_filename], check=True)
 
 def run_main_script_in_venv(args):
     main_script_url = "https://raw.githubusercontent.com/gesen2egee/dataset_tools/main/main_script.py"
     main_script_filename = "main_script.py"
 
-    download_file(main_script_url, main_script_filename)
+    if not os.path.exists(main_script_filename) or args.upgrade:
+        download_file(main_script_url, main_script_filename)
 
     if platform.system() == 'Windows':
         activate_script = os.path.join('venv', 'Scripts', 'activate.bat')
@@ -42,7 +44,8 @@ def run_main_script_in_venv(args):
         "--drop_colortag" if args.drop_colortag else "",
         "--clothtag" if args.clothtag else "",
         "--not_char" if args.not_char else "",
-        "--use_norm" if args.use_norm else "",
+        "--peopletag" if args.peopletag else "",
+        "--not_char" if args.not_char else "",
         f"--continue_caption {args.continue_caption}" if args.continue_caption else "",
         args.directory
     ]
@@ -64,8 +67,10 @@ if __name__ == "__main__":
     parser.add_argument("--drop_colortag", action="store_true", help="自動刪除顏色特徵標籤")
     parser.add_argument("--clothtag", action="store_true", help="自動處理服裝標籤")
     parser.add_argument("--not_char", action="store_true", help="目錄名不是角色")
-    parser.add_argument("--use_norm", action="store_true", help="忽略clip文字向量長度，標會較短")
+    parser.add_argument("--debiased", action="store_true", help="設置clip score上限減少florence偏差")
+    parser.add_argument("--peopletag", action="store_true", help="前置多人標籤(大多nsfw)")
     parser.add_argument("--continue_caption", type=int, default=0, help="忽略n天內打的標")
+    parser.add_argument("--upgrade", action="store_true", help="升級腳本")
     parser.add_argument("directory", type=str, help="處理目錄地址")
     args = parser.parse_args()
 
