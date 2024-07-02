bf74c6bdaabee1b24de4ea151e73286fc2134ed7
gesen2egee <79357052+gesen2egee@users.noreply.github.com>
2024-07-03 01:00:08 +0800

    Update caption.py

--- caption.py
+++ caption.py
@@ -21,8 +21,8 @@ def run_setup_script():
     setup_filename = "setup.py"
     main_script_filename = "main_script.py"
     
-    if not os.path.exists(setup_filename) or args.upgrade:
-        download_file(setup_url, setup_filename)
+    #if not os.path.exists(setup_filename) or args.upgrade:
+    #    download_file(setup_url, setup_filename)
     if not os.path.exists(main_script_filename) or args.upgrade:
         subprocess.run([sys.executable, setup_filename], check=True)
 
@@ -30,8 +30,8 @@ def run_main_script_in_venv(args):
     main_script_url = "https://raw.githubusercontent.com/gesen2egee/dataset_tools/main/main_script.py"
     main_script_filename = "main_script.py"
 
-    if not os.path.exists(main_script_filename) or args.upgrade:
-        download_file(main_script_url, main_script_filename)
+    #if not os.path.exists(main_script_filename) or args.upgrade:
+    #    download_file(main_script_url, main_script_filename)
 
     if platform.system() == 'Windows':
         activate_script = os.path.join('venv', 'Scripts', 'activate.bat')
@@ -39,6 +39,7 @@ def run_main_script_in_venv(args):
         activate_script = os.path.join('venv', 'bin', 'activate')
 
     command_args = [
+        args.directory,
         "--folder_name" if args.folder_name else "",
         "--drop_chartag" if args.drop_chartag else "",
         "--drop_colortag" if args.drop_colortag else "",
@@ -46,8 +47,9 @@ def run_main_script_in_venv(args):
         "--peopletag" if args.peopletag else "",
         "--not_char" if args.not_char else "",
         "--debiased" if args.debiased else "",
-        f"--continue_caption {args.continue_caption}" if args.continue_caption else "",
-        args.directory
+        "--rawdata" if args.rawdata else "",
+        f"--custom_keeptag=\"{args.custom_keeptag}\"" if args.custom_keeptag else "",
+        f"--continue_caption {args.continue_caption}" if args.continue_caption else ""
     ]
 
     # 过滤掉空字符串
@@ -69,6 +71,8 @@ if __name__ == "__main__":
     parser.add_argument("--not_char", action="store_true", help="目錄名不是角色")
     parser.add_argument("--debiased", action="store_true", help="設置clip score上限減少florence偏差")
     parser.add_argument("--peopletag", action="store_true", help="前置多人標籤(大多nsfw)")
+    parser.add_argument("--rawdata", action="store_true", help="大資料集")
+    parser.add_argument("--custom_keeptag", type=str, default=None, help="自定義自動留標")
     parser.add_argument("--continue_caption", type=int, default=0, help="忽略n天內打的標")
     parser.add_argument("--upgrade", action="store_true", help="升級腳本")
     parser.add_argument("directory", type=str, help="處理目錄地址")
